690afe74c6792d24ad5b4ab7f48f1e0712fb3269
Kathleen Robertson <kathleen.robertson@ama.ab.ca>
2015-11-07 10:33:10 -0700

    adding in design

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  root 'welcome#index'
-  
+  root 'pages#home'
+  get 'code-auth', to: 'pages#code-auth'
+  get 'form', to: 'pages#form'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
