c9f76354d3108daf42b7490366ad3a293e38ffd8
Ryan Jones <ryan@system88.com>
2015-11-06 19:27:15 -0700

    change spec to change for hi2u

--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -2,6 +2,6 @@ describe "Testing welcome page" do
   it "checks the welcome page" do
     visit root_path
 
-    expect(page).to have_content 'Hello World'
+    expect(page).to have_content 'hi2u'
   end
 end
