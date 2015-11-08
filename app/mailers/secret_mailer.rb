a31c3533720dca224b3ee679c9982b349ca54310
Michael van den Beuken <michael.beuken@gmail.com>
2015-11-08 10:46:52 -0700

    sendgrid_disable doesn't seem to work globally. Going back to the per-method method.

--- app/mailers/secret_mailer.rb
+++ app/mailers/secret_mailer.rb
@@ -1,9 +1,8 @@
 class SecretMailer < ApplicationMailer
   include SendGrid
 
-  sendgrid_disable :opentrack, :clicktrack, :ganalytics
-
   def new_secret_email(args = {})
+    sendgrid_disable :opentrack, :clicktrack, :ganalytics
     @secret = args.fetch(:secret, nil).try(:decorate)
     mail(to: @secret.recipient.email, subject: I18n.t("mailer.new_secret_email.subject"))
   end
