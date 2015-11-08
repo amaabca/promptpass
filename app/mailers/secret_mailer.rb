c0dd4864216acd17c22f24fffe633e161dbf89ef
Michael van den Beuken <michael.beuken@gmail.com>
2015-11-08 10:42:44 -0700

    Added SendGrid back. We are able to disable click and open tracking using SMTP headers.

--- app/mailers/secret_mailer.rb
+++ app/mailers/secret_mailer.rb
@@ -1,4 +1,7 @@
 class SecretMailer < ApplicationMailer
+  include SendGrid
+
+  sendgrid_disable :opentrack, :clicktrack, :ganalytics
 
   def new_secret_email(args = {})
     @secret = args.fetch(:secret, nil).try(:decorate)
